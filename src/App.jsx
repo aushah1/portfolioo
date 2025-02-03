2325a30db2af0462c831273bd5fbc77976fa7238
Aushah Gowhar <waniaushahgowhar@gmail.com>
2025-02-03 20:33:41 +0530

    Completed Portfolio

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import Footer from "./components/Footer";
+import Hero from "./components/Hero";
+import Navbar from "./components/Navbar";
+import Portfolio from "./components/Portfolio";
+import Skills from "./components/Skills";
+
+export default function App() {
+  return (
+    <>
+      <Navbar />
+      <Hero />
+      <Skills />
+      <Portfolio />
+      <Footer />
+    </>
+  );
+}
---
+++ src/components/Footer.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { FaGithubSquare, FaLinkedin } from "react-icons/fa";
+
+const Footer = () => {
+  return (
+    <div className="max-w-[1300px] mx-auto flex justify-between p-6 md:p-20 text-sm md:text-lg mt-12">
+      <div className=" flex items-center justify-center gap-4">
+        <h3 className="text-2xl text-gray-200 font-semibold">Aushah Gowhar</h3>
+        <div className="flex flex-row gap-6 items-center justify-center text-gray-400 text-4xl p-0 mt-0">
+          <a href="#">
+            <FaGithubSquare />
+          </a>
+          <a href="#">
+            <FaLinkedin />
+          </a>
+        </div>
+      </div>
+
+      <p className="text-gray-400">@2025 Aushah</p>
+    </div>
+  );
+};
+
+export default Footer;
